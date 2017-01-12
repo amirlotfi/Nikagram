775d61b71452682b9828662322be5e594fbe96a0
amirlotfi <amir.lotfi72@yahoo.com>
2017-01-12 14:10:26 +0330

    Update BuildVars.java

--- app/src/main/java/ir/nikagram/messenger/BuildVars.java
+++ app/src/main/java/ir/nikagram/messenger/BuildVars.java
@@ -5,7 +5,7 @@ public class BuildVars {
     public static boolean DEBUG_VERSION = false;
     public static int BUILD_VERSION = 851;
     public static String BUILD_VERSION_STRING = "3.13";
-    public static int APP_ID = 52066;
-    public static String APP_HASH = "fe99ef44a3e9dd9e79d726e5942d9ab2";
+    public static int APP_ID = ;
+    public static String APP_HASH = "";
     public static String SEND_LOGS_EMAIL = "amir.lotfi94@gmail.com";
 }
